c00d4923adad3838740a6feac445429da1d86db3
Aaron Reynoza <aaron@reynoza.org>
2025-08-16 22:37:38 -0600

    test githubci workflow

--- aws/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"  # Update this with your bucket name
-    key            = "aws-infra/terraform.tfstate"
-    region         = "us-east-1"  # Update with your preferred region
-    encrypt        = true
-    dynamodb_table = "terraform-locks"
-  }
-}
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "plex_ip" {
-  description = "IP address of the Plex container"
-  value       = proxmox_container.plex.ip_address
-}
-
-output "plex_container_id" {
-  description = "ID of the Plex container"
-  value       = proxmox_container.plex.id
-}
